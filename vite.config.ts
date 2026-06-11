e0d217938cd090f9e4af3726569c42510ea5e8d1
jouddagga <jouddagga083@gmail.com>
2026-06-11 23:22:54 +0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/joudahib-website1/",
+  base: "/joudahib-website3/",
   plugins: [
     react(),
     tailwindcss(),
